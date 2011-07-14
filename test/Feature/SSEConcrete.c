e158a00649ce8750fd2bc91bb797b6bd5156540b
istvan <istvan@istvan.(none)>
2011-07-14 16:54:20 +0200

    Added "pavg" test into SSEConcrete since it was added on top of KleeFP

--- test/Feature/SSEConcrete.c
+++ test/Feature/SSEConcrete.c
@@ -2,6 +2,7 @@
 // RUN: %klee --no-output --exit-on-error %t1.bc
 
 #include "xmmintrin.h"
+#include "emmintrin.h"
 #include "assert.h"
 #include "klee/klee.h"
 
@@ -24,16 +25,37 @@ void zlimit (int simd, float *src , float *dst , size_t size) {
 	}
 }
 
+void avg (int simd, uint8_t *src , uint8_t *src2, uint8_t *dst) {
+	if (simd) {
+		(*(__m128i*)dst) = _mm_avg_epu8((*(__m128i*)src), (*(__m128i*)src2));
+	}
+	else {
+		int size = 16;
+		while (size) {
+			*dst = ((*src)+(*src2))/2;
+			src++; src2++; dst++; size--;
+		}
+	}
+}
+
 int main(void) {
 	float src[64], dstv[64], dsts[64];
-	uint32_t *dstvi = (uint32_t*) dstv;
-	uint32_t *dstsi = (uint32_t*) dsts;
 	int i;
 	for(i=0; i<64; ++i)
 		src[i] = i-32;
 	zlimit (0, src, dsts, 64);
 	zlimit (1, src, dstv, 64);
 	for (i = 0; i < 64; ++i)
+		assert(dstv[i] == dsts[i]);
+
+	uint8_t srci_a[16], srci_b[16], dstvi[16], dstsi[16];
+	for(i=0; i<16; ++i)  {
+		srci_a[i] = i;
+		srci_b[i] = i+10;
+	}
+	avg (0, srci_a, srci_b, dstsi);
+	avg (1, srci_a, srci_b, dstvi);
+	for(i=0; i<16; ++i)
 		assert(dstvi[i] == dstsi[i]);
 }
 
